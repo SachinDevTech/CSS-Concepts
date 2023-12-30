94bdf6e10b527f158fa2ed81a09ca887d9d1af8b
Sachin Kumar <96971711+SachinDevTech@users.noreply.github.com>
2023-12-30 17:01:35 +0530

    Add files via upload

---
+++ Modern Card/script.js
@@ -0,0 +1,4 @@
+function sendWishes() {
+    alert("Best wishes for a wonderful New Year 2024!");
+    // You can add more functionality here like sending an email, sharing on social media, etc.
+  }
